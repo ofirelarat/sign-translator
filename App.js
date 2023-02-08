dc41dc03fa45b373bac94b4b029205ccfed720b0
ofirelarat <96ofir11@gmail.com>
2023-02-08 20:36:55 +0200

    app init

--- App.js
+++ App.js
@@ -1,10 +1,27 @@
+import {useState} from 'react'
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+// import { Camera } from 'react-native-vision-camera';
 
 export default function App() {
+  const [isCameraOpen, setIsCameraOpen] = useState(false);
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      {isCameraOpen ? (
+          <Text>Camera!</Text>
+        // <Camera style={styles.camera} />
+      ) : (
+        <View>
+          <Text style={styles.text}>Open the camera</Text>
+          <Button
+            title="Open camera"
+            onPress={() => setIsCameraOpen(true)}
+            color="#333"
+          />
+        </View>
+      )}
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +33,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  camera: {
+    height: '100%',
+    width: '100%',
   },
 });
